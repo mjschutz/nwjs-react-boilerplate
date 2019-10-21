90657790cf3c021eef8f60c4d0c1ed5f8a79dc23
Mauro Joel Schutz <mjschutz@outlook.com>
2019-10-20 22:31:38 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# NW.js React Boilerplate
+
+NW.js Boilerplate project that run Webpack, create a static/index.html and open it on a new NW.js window that runs a React code on src/index.js
+
+### Installing
+
+Fork this repository and rename it to match your project.
---
+++ main.js
@@ -0,0 +1,86 @@
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var pjson = require('./package.json');
+
+const staticDir = path.resolve(__dirname, 'static');
+
+if (!fs.existsSync(staticDir)){
+    fs.mkdirSync(staticDir);
+} else {
+	var removeDir = (dirPath) => {
+		if (fs.existsSync(dirPath)) {
+			return;
+		}
+
+		var list = fs.readdirSync(dirPath);
+		for (var i = 0; i < list.length; i++) {
+			var filename = path.join(dirPath, list[i]);
+			var stat = fs.statSync(filename);
+
+			if (filename == "." || filename == "..") {
+				// do nothing for current and parent dir
+			} else if (stat.isDirectory()) {
+				removeDir(filename);
+			} else {
+				fs.unlinkSync(filename);
+			}
+		}
+
+		fs.rmdirSync(dirPath);
+	};
+	removeDir(staticDir);
+}
+
+webpack({
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		publicPath: staticDir + '/',
+		path: staticDir,
+		filename: 'index.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: pjson.name,
+			publicPath: path.resolve(__dirname, 'public'),
+			template:'public/index.html',
+			filename: 'index.html'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					"style-loader",
+					"css-loader"
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: 'file-loader'
+			}
+		]
+	}
+}, (err, stats) => { // Stats Object
+	if (err || stats.hasErrors()) {
+		nw.Window.open('about:blank', {}, function(win) {
+			win.window.document.write('<pre>', stats, '</pre>');
+		});
+		return;
+	}
+
+	nw.Window.open('./static/index.html', {}, function(win) {});
+});
